0350801c641d04335a33e57bd637ab2ec7f41b5d
Ayushsanu <107394283+Ayushsanu@users.noreply.github.com>
2022-12-05 10:08:47 +0530

    Leetcode solution of remove duplicates from array

---
+++ RemoveDuplicateSolution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int j=1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i] != nums[j]){
+                count++;
+                nums[count] = nums[j];
+            }
+            j++;
+        }
+        return count+1;
+    }
+}
