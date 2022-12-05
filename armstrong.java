7826e8d2c153fab369cbc3a562670ff1b69fd710
Ayushsanu <107394283+Ayushsanu@users.noreply.github.com>
2022-12-05 09:53:25 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{Scanner sc=new Scanner(System.in);
+    System.out.println("enter the no :");
+   int N=sc.nextInt();
+   int M=N;
+   int reverse=0;
+   while (N!=0){
+     int remainder=N%10;
+     reverse=reverse*10+remainder;
+     N=N/10;
+   }
+   System.out.println(reverse);
+   if(M==reverse){
+     System.out.println("true");
+   }
+   else{
+     System.out.println("false");
+   }
+		//your code here
+	}
+}
