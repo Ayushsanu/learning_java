0db1c9ec0dee812c694172907c088e8c9d19541a
Ayushsanu <107394283+Ayushsanu@users.noreply.github.com>
2022-12-05 09:58:30 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class armstrong
 {
 	public static void main (String[] args) throws java.lang.Exception
-	{Scanner sc=new Scanner(System.in);
+	{Scanner sc=new Scanner(System.in); // this cod eis for taking input in n;
     System.out.println("enter the no :");
    int N=sc.nextInt();
    int M=N;
